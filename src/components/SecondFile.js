e43ecc5da1fc98f28b0fcb7c20d7471688aa3867
Sharmilee S <sharmilee.s@tarnea.com>
2023-04-12 13:05:39 +0530

    sw.js introduced

--- src/components/SecondFile.js
+++ src/components/SecondFile.js
@@ -2,23 +2,31 @@ import React from 'react'
 import logo from '../images/vector.jpg'
 
 function SecondFile() {
-    // navigator.serviceWorker.register('service-worker.js');
+    
+  navigator.serviceWorker.register("sw.js");
     const HandleClick=()=>{
-        Notification.requestPermission().then(perm =>{
-            if(perm === "granted"){
-               const notification = new Notification("Notification Message",{
-                    body:"Sub to tarnea",
-                    data:{hello:"world"},
-                    icon:logo,
-                    // tag:"Welcome Message"
-                })
-
-                // notification.addEventListener("close",)
-            // navigator.serviceWorker.ready.then(function(registration) {
-            // registration.showNotification('Notification with ServiceWorker');
-            // });
-            }
+        Notification.requestPermission(function(result) {
+        if (result === 'granted') {
+            navigator.serviceWorker.ready.then(function(registration) {
+            registration.showNotification('Notification with ServiceWorker');
+            });
+        }
         })
+// .then(perm =>{
+//             if(perm === "granted"){
+//             //    const notification = new Notification("Notification Message",{
+//             //         body:"Sub to tarnea",
+//             //         data:{hello:"world"},
+//             //         icon:logo,
+//             //         // tag:"Welcome Message"
+//             //     })
+
+//                 // notification.addEventListener("close",)
+//             navigator.serviceWorker.ready.then(function(registration) {
+//             registration.showNotification('Notification with ServiceWorker');
+//             });
+//             }
+//         })
     }
   return (
     <div>
